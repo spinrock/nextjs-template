79a5b66b0b52671ec5cda0c6077ea2f0bcb479ba
satoshi.fukai <chemistreal2@gmail.com>
2021-02-17 00:02:04 +0900

    Add Delete Todo TestCase

--- src/Todo/test/reducer.spec.ts
+++ src/Todo/test/reducer.spec.ts
@@ -1,9 +1,9 @@
 import {Todo} from '../interface' 
-import reducer, {addTodo, toggleTodo} from '../reducer'
+import reducer, {addTodo, toggleTodo, deleteTodo} from '../reducer'
 
 // Sample DataSet
 const todo00: Todo = {
-    id:"0",
+    id:'0',
     title: 'test',
     description: 'this is test description',
     completed: false,
@@ -12,9 +12,19 @@ const todo00: Todo = {
 }
 
 const todo01: Todo = {
-    id:"1",
+    id:'1',
     title: 'test1',
     description: 'this is test description1',
+    completed: false,
+    parentTaskId: null,
+    childrenTaskIds: []
+}
+
+
+const todo02: Todo = {
+    id:'2',
+    title: 'test2',
+    description: 'this is test description2',
     completed: false,
     parentTaskId: null,
     childrenTaskIds: []
@@ -78,4 +88,79 @@ describe('toggleTodo Test', () => {
         })).toEqual(expected)
     })
 
+    it ('Selected Todo is Not Found', () => {
+        const actual:Todo[] = [todo00, todo01]
+        const expected:Todo[] = [todo00, todo01]
+
+        expect(reducer(actual, {
+            type: toggleTodo.type, 
+            payload: {
+                id: todo02.id
+            }
+        })).toEqual(expected)
+    })
+})
+
+describe('deleteTodo Test', () => {
+    const threeTodos:Todo[] = [todo00, todo01, todo02]
+
+    it('Delete First Todo in Theree Todos', () => {
+        const actual: Todo[] = [...threeTodos]
+        const expected: Todo[] = [todo01, todo02]
+
+        expect(reducer(actual, {
+            type: deleteTodo.type,
+            payload: {
+                id: todo00.id
+            }
+        })).toEqual(expected)
+    })
+
+    it('Delete Second Todo in Three Todos', () => {
+        const actual: Todo[] = [...threeTodos]
+        const expected: Todo[] = [todo00, todo02]
+
+        expect(reducer(actual, {
+            type: deleteTodo.type,
+            payload: {
+                id: todo01.id
+            }
+        })).toEqual(expected)
+    })
+
+    it('Delete Last Todo in Three Todos', () => {
+        const actual: Todo[] = [...threeTodos]
+        const expected: Todo[] = [todo00, todo01]
+
+        expect(reducer(actual, {
+            type: deleteTodo.type,
+            payload: {
+                id: todo02.id
+            }
+        })).toEqual(expected)
+    })
+
+    it('Delete Final Todo in Todos', () => {
+        const actual: Todo[] = [todo00]
+        const expected: Todo[] = []
+
+        expect(reducer(actual, {
+            type: deleteTodo.type,
+            payload: {
+                id: todo00.id
+            }
+        })).toEqual(expected)
+    })
+
+    it('Delete Todo is Not Founde', () => {
+        const actual:Todo[] = [todo00, todo01]
+        const expected:Todo[] = [todo00, todo01]
+
+        expect(reducer(actual, {
+            type: deleteTodo.type, 
+            payload: {
+                id: todo02.id
+            }
+        })).toEqual(expected)
+    })
 })
