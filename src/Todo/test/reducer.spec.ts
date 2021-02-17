7c002197fc64e1c6c7521023221ab37f9e4c11d7
satoshi.fukai <chemistreal2@gmail.com>
2021-02-17 18:41:02 +0900

    Add UpdateTodo Testcode

--- src/Todo/test/reducer.spec.ts
+++ src/Todo/test/reducer.spec.ts
@@ -1,33 +1,33 @@
 import {Todo} from '../interface' 
-import reducer, {addTodo, toggleTodo, deleteTodo} from '../reducer'
+import reducer, {addTodo, toggleTodo, deleteTodo, updateTodo} from '../reducer'
 
 // Sample DataSet
 const todo00: Todo = {
-    id:'0',
-    title: 'test',
-    description: 'this is test description',
-    completed: false,
-    parentTaskId: null,
-    childrenTaskIds: []
+  id:'0',
+  title: 'test',
+  description: 'this is test description',
+  completed: false,
+  parentTaskId: null,
+  childrenTaskIds: []
 }
 
 const todo01: Todo = {
-    id:'1',
-    title: 'test1',
-    description: 'this is test description1',
-    completed: false,
-    parentTaskId: null,
-    childrenTaskIds: []
+  id:'1',
+  title: 'test1',
+  description: 'this is test description1',
+  completed: false,
+  parentTaskId: null,
+  childrenTaskIds: []
 }
 
 
 const todo02: Todo = {
-    id:'2',
-    title: 'test2',
-    description: 'this is test description2',
-    completed: false,
-    parentTaskId: null,
-    childrenTaskIds: []
+  id:'2',
+  title: 'test2',
+  description: 'this is test description2',
+  completed: false,
+  parentTaskId: null,
+  childrenTaskIds: []
 }
 
 
@@ -102,65 +102,166 @@ describe('toggleTodo Test', () => {
 })
 
 describe('deleteTodo Test', () => {
-    const threeTodos:Todo[] = [todo00, todo01, todo02]
+  const threeTodos:Todo[] = [todo00, todo01, todo02]
 
-    it('Delete First Todo in Theree Todos', () => {
-        const actual: Todo[] = [...threeTodos]
-        const expected: Todo[] = [todo01, todo02]
+  it('Delete First Todo in Theree Todos', () => {
+    const actual: Todo[] = [...threeTodos]
+    const expected: Todo[] = [todo01, todo02]
 
-        expect(reducer(actual, {
-            type: deleteTodo.type,
-            payload: {
-                id: todo00.id
-            }
-        })).toEqual(expected)
-    })
+    expect(reducer(actual, {
+      type: deleteTodo.type,
+      payload: {
+        id: todo00.id
+      }
+    })).toEqual(expected)
+  })
 
-    it('Delete Second Todo in Three Todos', () => {
-        const actual: Todo[] = [...threeTodos]
-        const expected: Todo[] = [todo00, todo02]
+  it('Delete Second Todo in Three Todos', () => {
+    const actual: Todo[] = [...threeTodos]
+    const expected: Todo[] = [todo00, todo02]
 
-        expect(reducer(actual, {
-            type: deleteTodo.type,
-            payload: {
-                id: todo01.id
-            }
-        })).toEqual(expected)
-    })
+    expect(reducer(actual, {
+      type: deleteTodo.type,
+      payload: {
+        id: todo01.id
+      }
+    })).toEqual(expected)
+  })
 
-    it('Delete Last Todo in Three Todos', () => {
-        const actual: Todo[] = [...threeTodos]
-        const expected: Todo[] = [todo00, todo01]
+  it('Delete Last Todo in Three Todos', () => {
+    const actual: Todo[] = [...threeTodos]
+    const expected: Todo[] = [todo00, todo01]
 
-        expect(reducer(actual, {
-            type: deleteTodo.type,
-            payload: {
-                id: todo02.id
-            }
-        })).toEqual(expected)
-    })
+    expect(reducer(actual, {
+      type: deleteTodo.type,
+      payload: {
+        id: todo02.id
+      }
+    })).toEqual(expected)
+  })
 
-    it('Delete Final Todo in Todos', () => {
-        const actual: Todo[] = [todo00]
-        const expected: Todo[] = []
+  it('Delete Final Todo in Todos', () => {
+    const actual: Todo[] = [todo00]
+    const expected: Todo[] = []
 
-        expect(reducer(actual, {
-            type: deleteTodo.type,
-            payload: {
-                id: todo00.id
-            }
-        })).toEqual(expected)
-    })
+    expect(reducer(actual, {
+      type: deleteTodo.type,
+      payload: {
+        id: todo00.id
+      }
+    })).toEqual(expected)
+  })
 
-    it('Delete Todo is Not Founde', () => {
-        const actual:Todo[] = [todo00, todo01]
-        const expected:Todo[] = [todo00, todo01]
+  it('Delete Todo is Not Founde', () => {
+    const actual:Todo[] = [todo00, todo01]
+    const expected:Todo[] = [todo00, todo01]
 
-        expect(reducer(actual, {
-            type: deleteTodo.type, 
-            payload: {
-                id: todo02.id
-            }
-        })).toEqual(expected)
-    })
+    expect(reducer(actual, {
+      type: deleteTodo.type, 
+      payload: {
+        id: todo02.id
+      }
+    })).toEqual(expected)
+  })
+})
+
+describe('updateTodo Test', () => {
+  it('Update title', () => {
+    const actual = [todo00, todo01]
+    const expected = [todo00, {...todo01, title:"update test1"}]
+    expect(reducer(actual, {
+      type: updateTodo.type, 
+      payload: {
+        id: todo01.id,
+        title: "update test1"
+      }
+    })).toEqual(expected)
+  })
+
+  it('Update description', () => {
+    const actual = [todo00, todo01]
+    const expected = [{...todo00, description:"this is update description"}, todo01]
+    expect(reducer(actual, {
+      type: updateTodo.type, 
+      payload: {
+        id: todo00.id,
+        description: "this is update description"
+      }
+    })).toEqual(expected)
+  })
+
+  it('Update completed', () => {
+    // completed value update via toggleTodo
+    // this action is not used
+  })
+
+  it('Update parentTaskId', () => {
+    const actual = [todo00, todo01]
+    const expected = [{...todo00, parentTaskId:todo01.id}, todo01]
+    expect(reducer(actual, {
+      type: updateTodo.type, 
+      payload: {
+        id: todo00.id,
+        parentTaskId: todo01.id
+      }
+    })).toEqual(expected)
+  })
+
+  it('Update childrenTaskId', () => {
+    const actual = [todo00, todo01, todo02]
+    const expected = [todo00, todo01, {...todo02, childrenTaskIds: [todo00.id, todo01.id]}]
+    expect(reducer(actual, {
+      type: updateTodo.type, 
+      payload: {
+        id: todo02.id,
+        childrenTaskIds: todo01.id
+      }
+    })).toEqual(expected)
+  })
+
+  it('Update AllValue', () => {
+    const actual = [todo00, todo01, todo02]
+    const expected = [todo00, todo01, {...todo02, title:"update test2", description:"this is update description2" , parentTaskId: todo00.id, childrenTaskIds: [todo01.id]}]
+    expect(reducer(actual, {
+      type: updateTodo.type, 
+      payload: {
+        id: todo02.id,
+        title: "update test2",
+        description: "this is update description2",
+        parentTaskId: todo00.id,
+        childrenTaskIds: [todo01.id]
+      }
+    })).toEqual(expected)
+  })
+
+  it('Update null value', () => {
+    const actual = [todo00, todo01, {...todo02, parentTaskId: todo00.id, childrenTaskIds:[todo01.id]}]
+    const expected = [todo00, todo01, todo02]
+    expect(reducer(actual, {
+      type: updateTodo.type, 
+      payload: {
+        id: todo02.id,
+        title: null,
+        description: null,
+        parentTaskId: null,
+        childrenTaskIds: null
+      }
+    })).toEqual(expected)
+  })
+
+
+  it('No Update undefined value', () => {
+    const actual = [todo00, todo01]
+    const expected = [todo00, todo01]
+    expect(reducer(actual, {
+      type: updateTodo.type, 
+      payload: {
+        id: todo01.id,
+        title: undefined,
+        description: undefined,
+        parentTaskId: undefined,
+        childrenTaskIds: undefined
+      }
+    })).toEqual(expected)
+  })
 })
