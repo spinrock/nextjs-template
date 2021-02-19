00055e4dde0d01382310c9d904bb5c3d1bcaf8f1
satoshi.fukai <chemistreal2@gmail.com>
2021-02-19 13:59:33 +0900

    Update TestCase

--- src/test/pages/index.test.tsx
+++ src/test/pages/index.test.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import { render, fireEvent } from '../testUtils'
-import { Home } from '../../pages/index'
+import { render } from '../testUtils'
+import Home from '../../pages/index'
+import { Provider } from 'react-redux'
+import store from '../../store'
 
 describe('Home page', () => {
   it('matches snapshot', () => {
-    const { asFragment } = render(<Home />, {})
+    const { asFragment } = render(
+      <Provider store={store}>
+        <Home />
+      </Provider>,
+      {}
+    )
     expect(asFragment()).toMatchSnapshot()
-  })
-
-  it('clicking button triggers alert', () => {
-    const { getByText } = render(<Home />, {})
-    window.alert = jest.fn()
-    fireEvent.click(getByText('Test Button'))
-    expect(window.alert).toHaveBeenCalledWith('With typescript and Jest')
   })
 })
