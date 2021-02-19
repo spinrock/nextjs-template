47fd42d616354f32492ce3120a0b2952ea9109f8
satoshi.fukai <chemistreal2@gmail.com>
2021-02-19 11:53:52 +0900

    Prepare Using Material-UI on Next.js

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,47 @@
 import * as React from 'react'
 import { Provider } from 'react-redux'
+import Head from 'next/head'
+import { ThemeProvider } from '@material-ui/core/styles'
+import CommonPageTemplate from '../components/CommonPageTemplate'
+import theme from '../styles/theme'
 import store from '../store'
+import PropTypes from 'prop-types'
+import CssBaseline from '@material-ui/core/CssBaseline'
 
-function MyApp({ Component, pageProps }) {
+export default function MyApp(props) {
+  const { Component, pageProps } = props
+
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side')
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles)
+    }
+  }, [])
+
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
+    <React.Fragment>
+      <Head>
+        <title>Next.js Template(TodoList)</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          <CommonPageTemplate>
+            <Component {...pageProps} />
+          </CommonPageTemplate>
+        </ThemeProvider>
+      </Provider>
+    </React.Fragment>
   )
 }
 
-export default MyApp
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+}
