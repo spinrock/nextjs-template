21f28acb54b22346f5d9c1385377a129378dd6c9
spinrock <chemistreal2@gmail.com>
2021-03-24 14:54:30 +0900

    Add Delete Todo without confirmation

--- src/Todo/components/TodoList/components/TodoItem.tsx
+++ src/Todo/components/TodoList/components/TodoItem.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 import { makeStyles } from '@material-ui/core/styles'
 import Card from '@material-ui/core/Card'
+import CardActionArea from '@material-ui/core/CardActionArea'
 import CardContent from '@material-ui/core/CardContent'
 import CheckBox from '@material-ui/core/Checkbox'
+import DeleteIcon from '@material-ui/icons/Delete';
+import IconButton from '@material-ui/core/IconButton'
 import Typography from '@material-ui/core/Typography'
 import theme from '../../../../styles/theme'
 import { Todo } from '../../../interface'
-import { toggleTodo } from '../../../reducer'
+import { toggleTodo, deleteTodo } from '../../../reducer'
 
 const useStyles = makeStyles({
   todoItem: {
@@ -36,27 +39,40 @@ const TodoItem: React.FC<Props> = ({ todo }: Props) => {
     dispatch(toggleTodo({ id: todo.id }))
   }
 
+  const deleteTodoFunc = () => {
+    dispatch(deleteTodo({ id: todo.id }))
+  }
+
   return (
     <Card className={classes.todoItem}>
-      <CardContent className={classes.cardContent}>
-        <Typography
-          variant="h5"
-          style={{
-            textDecorationLine: todo.completed ? 'line-through' : 'blink',
-          }}
-        >
-          {todo.title}
-        </Typography>
-        <Typography
-          variant="body1"
-          style={{
-            textDecorationLine: todo.completed ? 'line-through' : 'blink',
-          }}
-        >
-          {todo.description}
-        </Typography>
-      </CardContent>
       <CheckBox checked={todo.completed} onChange={toggleTodoFunc} />
+      <CardActionArea
+      >
+        <CardContent className={classes.cardContent}>
+          <Typography
+            variant="h5"
+            style={{
+              textDecorationLine: todo.completed ? 'line-through' : 'blink',
+            }}
+          >
+            {todo.title}
+          </Typography>
+          <Typography
+            variant="body1"
+            noWrap={true}
+            style={{
+              textDecorationLine: todo.completed ? 'line-through' : 'blink',
+            }}
+          >
+            {todo.description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <IconButton
+        onClick={deleteTodoFunc}
+      >
+        <DeleteIcon />
+      </IconButton>
     </Card>
   )
 }
