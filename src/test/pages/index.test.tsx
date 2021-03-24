1b04508aefb2fc98059d3cded61b068bb0e1ff71
spinrock <chemistreal2@gmail.com>
2021-03-24 10:47:36 +0900

    Overload DefaultRootState

--- src/test/pages/index.test.tsx
+++ src/test/pages/index.test.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { render } from '../testUtils'
 import Home from '../../pages/index'
 import { Provider } from 'react-redux'
-import store from '../../store'
+import store from '../../rootStore'
 
 describe('Home page', () => {
   it('matches snapshot', () => {
