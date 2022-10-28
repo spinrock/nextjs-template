d33f93bbabb3802f0f47e3ad92679599de658fe2
spinrock <chemistreal2@gmail.com>
2022-10-28 11:03:04 +0900

    Update State type

--- src/rootStore.ts
+++ src/rootStore.ts
@@ -1,14 +1,9 @@
-import 'react-redux'
 import { configureStore } from '@reduxjs/toolkit'
 import reducer from './rootReducer'
 import { TodoState } from './Todo/store'
 
 export type StoreState = {
   todo: TodoState
-}
-
-declare module 'react-redux' {
-  interface DefaultRootState extends StoreState {}
 }
 
 const store = configureStore({ reducer: reducer })
