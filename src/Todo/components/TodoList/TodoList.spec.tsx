85a8a30c5729b7b12825e37bc5492491197bfdab
spinrock <chemistreal2@gmail.com>
2023-03-24 10:08:06 +0000

    Add React import into each component test

--- src/Todo/components/TodoList/TodoList.spec.tsx
+++ src/Todo/components/TodoList/TodoList.spec.tsx
@@ -1,6 +1,7 @@
 /**
  * @jest-environment jsdom
  */
+import React from 'react';
 import { render, screen } from '../../../test/testUtils';
 import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event';
