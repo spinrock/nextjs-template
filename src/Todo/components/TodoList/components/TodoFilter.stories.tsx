cf15bc5411270376c343c17c3fa2e4f2a5a08e78
satoshi.fukai <chemistreal2@gmail.com>
2023-04-25 01:09:32 +0900

    Add TodoFilter Story

---
+++ src/Todo/components/TodoList/components/TodoFilter.stories.tsx
@@ -0,0 +1,38 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Provider } from 'react-redux';
+import store from '../../../../rootStore';
+import TodoFilter from './TodoFilter';
+
+const meta: Meta<typeof TodoFilter> = {
+  title: 'Todo/TodoFilter',
+  component: TodoFilter,
+  tags: ['autodocs'],
+  argTypes: {
+    filterState: {
+      options: [ null, true, false ],
+      control: { type: 'radio' },
+    }
+  },
+  decorators: [(story) => <Provider store={store}>{story()}</Provider>],
+};
+
+export default meta;
+type Story = StoryObj<typeof TodoFilter>;
+
+export const Default: Story = {
+  args: {
+    filterState: null,
+  },
+};
+
+export const Incompleted: Story = {
+  args: {
+    filterState: false,
+  },
+};
+
+export const Completed: Story = {
+  args: {
+    filterState: true,
+  },
+};
