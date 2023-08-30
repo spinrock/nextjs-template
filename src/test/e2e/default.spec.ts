22013e1a7851e0e7b11bd893ea898136cd1b41d9
satoshi.fukai <chemistreal2@gmail.com>
2023-08-31 00:25:59 +0900

    Add filter Todo Test

--- src/test/e2e/default.spec.ts
+++ src/test/e2e/default.spec.ts
@@ -5,11 +5,17 @@ test.beforeEach(async ({ page }) => {
 });
 
 test('has title', async ({ page }) => {
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle("Next.js Template(TodoList)");
+  // Arrange
+  const expectedTitle = "Next.js Template(TodoList)"
+
+  // Act
+
+  // Assert
+  await expect(page).toHaveTitle(expectedTitle);
 });
 
 test('has header', async ({ page }) => {
+  // Arrange
   const expectedHeaderTitle = "This is Next.js Template";
 
   // Act
