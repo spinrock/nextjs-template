caa1f63f9a8629e70c92aebfe15f81d5f1807f7d
satoshi.fukai <chemistreal2@gmail.com>
2024-02-17 01:11:51 +0900

    Update Shadow in Header

--- src/components/CommonPageTemplate/Header.tsx
+++ src/components/CommonPageTemplate/Header.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 const Header: React.FC<Props> = ({ title }: Props) => {
   return (
-    <div className="h-16 w-full bg-slate-200">
+    <div className="h-16 w-full bg-slate-200 shadow-md">
       <p className="text-2xl leading-[64px] pl-4" data-testid="header-title">
         {title}
       </p>
