c83739cc18bb987a1342a1f25238f4d187555c8a
satoshi.fukai <chemistreal2@gmail.com>
2024-10-31 00:13:54 +0900

    FSD構成のpages配下にapp/page.tsxの中身を移動

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,6 @@
-'use client'
+import React from 'react';
+import TaskListPage from '../src/pages/tasksList';
 
-import React from 'react'
-import AddTodoBtn from '../src/Todo/components/AddTodoBtn'
-import TodoList from '../src/Todo/components/TodoList'
-
-const IndexPage: React.FC = () => (
-  <div className="md:mx-36 mx-4">
-    <TodoList />
-    <AddTodoBtn />
-  </div>
-)
+const IndexPage: React.FC = () => (<TaskListPage />);
 
 export default IndexPage
